d7a243083f84d4a7dc5f8fb2b674f1bd81d49e89
“nishantrana2” <nishant.mailbag@gmail.com>
2019-08-23 17:46:53 +0530

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const forecast = require('./utils/forecast')
 
 const app = express()
 
-const port = proccess.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 
 const publicDirectoryPath = path.join(__dirname,'../public')
